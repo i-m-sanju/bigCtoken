6b48e24a436e8443b4dfcf56d68762991d6e444b
i-m-sanju <sanjayk2310btechcse2023@kccitm.edu.in>
2022-03-31 16:35:24 +0530

    Added fecility to transfer and approve tokens by somwone else

--- contracts/CToken.sol
+++ contracts/CToken.sol
@@ -13,7 +13,17 @@ contract CToken{
       address indexed _from, address indexed _to, uint256 _value
     );
 
+    event Approval(
+      address indexed _owner,
+      address indexed _spender,
+      uint256 _value
+    );
+
     mapping(address => uint256) public balanceof;
+    mapping(address => mapping(address => uint256)) public allowance;
+              //I               account b    t approved
+  
+  
   constructor(uint256 _initialSupply) public{
     balanceof[msg.sender]=_initialSupply;
     totalSupply = _initialSupply;
@@ -33,4 +43,37 @@ contract CToken{
       return true;
 
     } 
+
+    //delegated transfer : useful for exchanges like you allow an exchange to sell your token on your behalf and you just have to approve the txn
+
+    //approve function
+
+    function approve(address _spender, uint256 _value) public returns (bool success){
+      allowance[msg.sender][_spender] = _value;
+      
+      emit Approval(msg.sender, _spender, _value);
+      
+
+
+      return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){
+
+      //Require _from has enough token
+      require(_value <= balanceof[_from]);
+      //require allowance to be big enough to transfer funds
+      require(_value <= allowance[_from][msg.sender]);
+      //change balance
+      balanceof[_from] -= _value;
+      balanceof[_to] += _value;
+      //update allowance
+      //transfer event
+
+       emit Transfer(_from, _to, _value);
+      // return boolean 
+
+      return true;
+    }
+
 }
