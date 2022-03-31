40b4e51da81e8ec5cf27c0c7d06698232eeacf69
i-m-sanju <sanjayk2310btechcse2023@kccitm.edu.in>
2022-03-31 11:34:21 +0530

    testing if the account has enough funds

--- contracts/CToken.sol
+++ contracts/CToken.sol
@@ -2,13 +2,22 @@ pragma solidity ^0.8.0;
 
 contract CToken{
     uint256 public totalSupply;
-    string public name="BigC";
-    string public abv="BC";
+    string public name="BigC Token";
+    string public symbol="BC";
+    string public standard = "BC v1.01";
     //constructor
     //set the tokens 
     //read total number of tokens
-  constructor() public{
-      totalSupply = 10000;
 
+    mapping(address => uint256) public balanceof;
+  constructor(uint256 _initialSupply) public{
+    balanceof[msg.sender]=_initialSupply;
+    totalSupply = _initialSupply;
+  //allocate the initial supply to 1 address
+    }
+
+    //trnasfer
+    function transfer(address _to, uint256 _value) public returns(bool success){
+      require(balanceof[msg.sender] >= _value);
     }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 const CToken = artifacts.require("./CToken.sol");
 
 module.exports = function (deployer) {
-  deployer.deploy(CToken);
+  deployer.deploy(CToken, 1000);
 };
