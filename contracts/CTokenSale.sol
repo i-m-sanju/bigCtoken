cc54e3ff07095a56659d6d4bb3c093a42907fbf3
i-m-sanju <sanjayk2310btechcse2023@kccitm.edu.in>
2022-04-14 19:26:17 +0530

    made 75% of tokens availlable

--- contracts/CTokenSale.sol
+++ contracts/CTokenSale.sol
@@ -14,7 +14,15 @@ contract CTokenSale{
        tokenContract= _tokenContract;
        tokenPrice = _tokenPrice;
    }
+
+    function multiply(uint x, uint y) internal pure returns (uint z){
+        require(y==0 || (z= x*y)/y ==x);
+    }
+
     function buyTokens(uint256 _numberofTokens) public payable{
+        require(msg.value == multiply(_numberofTokens, tokenPrice));
+        require(tokenContract.balanceof(this)>= _numberofTokens);
+         
         tokenSold += _numberofTokens;
        emit Sell(msg.sender, _numberofTokens);
     }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,7 +2,7 @@ const CToken = artifacts.require("./CToken.sol");
 const CTokenSale = artifacts.require("./CTokenSale.sol");
 
 module.exports = function (deployer) {
-  deployer.deploy(CToken, 1000).then(function(){
+  deployer.deploy(CToken, 1000000).then(function(){
     var tokenPrice = 1000000000000000;
     return deployer.deploy(CTokenSale, CToken.address, tokenPrice);
   });
